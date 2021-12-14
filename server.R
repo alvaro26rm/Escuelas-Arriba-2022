24596ad238a10ba3853781d0c11795d1498f1307
alvaro26rm <83990581+alvaro26rm@users.noreply.github.com>
2021-12-14 17:56:38 -0300

    Update server.R

--- server.R
+++ server.R
@@ -11,10 +11,10 @@ server <- function(input, output, session) {
   output$map <- renderLeaflet({
     leaflet() %>%
       addTiles()%>%
-      setView(lng = -70.76293, lat = -33.52341, zoom = 10)
+      setView(lng = -70.76293, lat = -33.52341, zoom = 11)
     
   })
-
+  
   # This observer is responsible for maintaining the circles and legend,
   # according to the variables the user has chosen to map to color and size.
   observe({
@@ -26,17 +26,20 @@ server <- function(input, output, session) {
       # the values are categorical instead of continuous.
       colorData <- ifelse(eadata$EA_2021 == "SI", "SI", "NO")
       pal <- colorFactor(c("navy", "red"), colorData)
-    } else {
-      colorData <- eadata[[colorBy]]
-      pal <- colorBin("Dark2", colorData, 7, pretty = FALSE)
     }
+     else {
+      colorData <- eadata[[colorBy]]
+      pal <- colorFactor(c("orange", "purple"), colorData)
+    }
+    
     
     if (sizeBy == "EA_2021") {
       # Radius is treated specially in the "superzip" case.
       radius <- ifelse(eadata$EA_2021 == "SI", 1000, 1000)
-    } else {
-      radius <- eadata[[sizeBy]] / max(eadata[[sizeBy]]) * 1000
+    }  else {
+      radius <- eadata[[sizeBy]] * 10
     }
+    
     
     leafletProxy("map", data = eadata) %>%
       clearShapes() %>%
@@ -52,7 +55,7 @@ server <- function(input, output, session) {
     content <- as.character(tagList(
       tags$h4("", as.character(selectedEA$ESTABLECIMIENTO)),
       tags$strong(HTML(sprintf("%s, %s",
-                               selectedEA$COMUNA, selectedEA$DEPROV
+                               selectedEA$COMUNA, selectedEA$REGIÓN
       ))), tags$br(),
       sprintf("RBD: %s", selectedEA$RBD), tags$br(),
       sprintf("Matrícula total: %s", selectedEA$MATRÍCULA), tags$br(),
@@ -101,7 +104,8 @@ server <- function(input, output, session) {
                          selected = stillSelected, server = TRUE)
   })
   
-
+  
+  
   observe({
     if (is.null(input$goto))
       return()
@@ -123,16 +127,16 @@ server <- function(input, output, session) {
       filter(
         is.null(input$region) | REGIÓN %in% input$region,
         is.null(input$comuna) | COMUNA %in% input$comuna,
-        is.null(input$escuela) | RBD %in% input$escuela
+        is.null(input$escuela) | ESTABLECIMIENTO %in% input$escuela
       ) %>%
       mutate(Action = paste('<a class="go-map" href="" data-lat="', LATITUD, '" data-long="', LONGITUD, '" data-zip="', RBD , '"><i class="fa fa-crosshairs"></i></a>', sep=""))
     action <- DT::dataTableAjax(session, df, outputId = "eatabla")
     
     DT::datatable(df, options = list(ajax = list(url = action)), escape = FALSE)
+    
+    
+  }  )
   
-  
-}  )
-
 }
 
 
